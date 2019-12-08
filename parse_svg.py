ff9f26ec7dd65b869a6bddae6eb8b7e125b68942
David King <davidcking.inbox@gmail.com>
2019-12-07 18:05:32 -0700

    parse_svg.py: deleting all debug print statements, but not error or output

--- parse_svg.py
+++ parse_svg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+from sympy import *
+from sympy.geometry import *
 
 filename=sys.argv[1]
 
@@ -32,8 +34,6 @@ def rectify(rects, parent):
             class_count[ rect_class ] = []
 
         class_count[ rect_class ].append( rect )
-
-    print("about to merge %d rects from parent:" % len(rects), parent, parent.attrib, file=sys.stderr)
 
     for rect_class, rect_elements in class_count.items():
 
@@ -71,7 +71,6 @@ def list_to_svg_path_d(l):
             print(command, point, file=sys.stderr)
             raise
 
-    print(d, file=sys.stderr)
     return d + 'z'
 
 def svg_rect_to_coords( rect ):
@@ -94,7 +93,6 @@ def svg_rect_to_coords( rect ):
                ( round(x+width, precision), round(y+height, precision) ),
                ( round(x+width, precision), y) ]
 
-    print(coords, file=sys.stderr)
     return coords
 
 def merge_rect_into_polygon(svg_rect, svg_parent, polygon):
@@ -105,10 +103,9 @@ def merge_rect_into_polygon(svg_rect, svg_parent, polygon):
     match_point = None
     points_to_insert = []
     i = -1
-    for corner in rect:
+    for which_corner, corner in zip(['ll','ul','ur','lr'], rect):
         try:
             i = polygon.index( corner )
-            print('found:', corner, "==", polygon[i])
         except:
             points_to_insert.append( corner )
             continue
@@ -116,35 +113,21 @@ def merge_rect_into_polygon(svg_rect, svg_parent, polygon):
         if match_point == None: 
             match_point = i
 
-        #print('match_point:', match_point)
-
+        # delete point from polygon
         polygon.pop(i)
 
-    #if i == -1: # no overlap
-        #print("polygon does not overlap rect", file=sys.stderr)
-        #print("polygon:", polygon, file=sys.stderr)
-        #print("rect:", rect, file=sys.stderr)
-        #sys.exit(1)
-        #return polygon
 
     if i >= 0:
         # delete the rect 
         try:
             svg_parent.remove(svg_rect)
-            #for rect in rect_elements: 
-                #parent.remove(rect)
         except:
-            #print('type(parent)', type(parent), file=sys.stderr)
-            #print('parent', parent, parent.tag, parent.attrib, file=sys.stderr)
-            print('rect', rect, rect.attrib, file=sys.stderr)
-            print("error:", sys.exc_info()[0], file=sys.stderr)
             sys.exit(1)
 
         for new_point in points_to_insert:
             polygon.insert( i, new_point )
             i += 1
             
-    print(polygon, file=sys.stderr)
     return polygon
 
 def examine_rect(element, parent, data):
@@ -155,11 +138,6 @@ def examine_rect(element, parent, data):
         nrects = len(rects)
         if nrects > 1:
             data['nrects'] = nrects
-            print(data)
-
-            if parent: print(parent.tag, parent.attrib)
-
-            print('class(rects[0])', type(rects[0]), file=sys.stderr)
             rectify(rects,element)
     
 #for i,child in enumerate(root):
@@ -167,9 +145,6 @@ def examine_rect(element, parent, data):
 #   traverse( child, root, examine_rect)
 
 for i,group in enumerate(root.iter(NS + 'g')):
-    print('type(root)', type(root), file=sys.stderr)
     traverse( group, root, examine_rect, {'path': 'svg'})
-
-
 
 tree.write('processed.svg')
